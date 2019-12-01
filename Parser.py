9b6735802fd69537cb381f4750684e5bdc6078b5
milliondegree <zhangjiachi1007@gmail.com>
2019-12-01 00:53:45 -0500

    parser v2

--- Parser.py
+++ Parser.py
@@ -1,4 +1,17 @@
 
+SYMBOL_DICT = {
+  '=': "equal",
+  '!=': "not_equal",
+  'in': "in",
+  'inside': "inside",
+  '>': "greater",
+  '<': "smaller",
+  '>=': "greater_equal",
+  '<=': "smaller_equal",
+  'like': "like"
+}
+
+
 def standardize(strings):
   res = []
   for string in strings:
@@ -8,16 +21,36 @@ def standardize(strings):
 
 
 def parseAction(string):
-  strings = standardize(string.split(" "))
+  pass
 
 
-def parseCondition(string):
+def __parseSingleCondition(statement):
+  tokens = standardize(statement.strip().split(" "))
+  assert len(tokens)>=3, "wrong number of tokens in condition "+statement
+
+  ifNot = False
+  if tokens[0]=="not":
+    ifNot = True
+  pass
+
+
+
+def parseCondition(statement):
   '''
   input: a string after "where"
-  output: return a 3-layer dictionary: or dict, and dict, condition dict
+  output: return a 2-layer lists: or list, and list whose elements are condition dict
   condition dict: key is table.attribute, value is function
+  We assume that this statement does not contain "()".
   '''
-  pass
+  res = []
+  cnfStrs = statement.split("or")
+  for cnfStr in cnfStrs:
+    cnf = []
+    conditions = cnfStr.split("and")
+    for condition in conditions:
+      cnf.append(__parseSingleCondition(condition))
+    res.append(cnf)
+  return res
 
 
 def parse(statement):
@@ -38,8 +71,6 @@ def parse(statement):
     res['conditions'] = parseCondition(conditions)
 
   return res
-  
-
 
 
 if __name__ == '__main__':
