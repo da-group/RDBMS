9e1a158ed99a22c1c75d9f2106cb876ef6cc77c0
Zheyi Wang <36946721+wineinpink@users.noreply.github.com>
2019-12-03 10:23:21 -0500

    新增merge scan, 大幅减少join时间
    join函数中根据两边表tuple数量来选择用nested loop join还是merge scan

--- Database.py
+++ Database.py
@@ -3,6 +3,8 @@ from Attribute import *
 from Condition import *
 from Parser import *
 import pickle
+import time
+import math
 
 class Database():
     def __init__(self, name):
@@ -68,46 +70,89 @@ class Database():
         for joinParam in joinParams:
             # 解析joinCondition
             s1 = joinParam[0].split('.')
-            table1 = self.getTableByName(s1[0])
-            attrName1 = s1[1]
-            s2 = joinParam[2].split('.')
-            table2 = self.getTableByName(s2[0])
-            attrName2 = s2[1]
-            assert table1.name in self.tables.keys() and table2.name in self.tables.keys(), "Wrong join tables"
-            assert attrName1 in table1.attributes.keys() and attrName2 in table2.attributes.keys(), "Wrong join attributes"
             funcName = joinParam[1]
+            s2 = joinParam[2].split('.')
+            t1 = self.getTableByName(s1[0])
+            t2 = self.getTableByName(s2[0])
+            attrName1 = s1[1]
+            attrName2 = s2[1]
+            assert t1.name in self.tables.keys() and t2.name in self.tables.keys(), "Wrong join tables"
+            assert attrName1 in t1.attributes.keys() and attrName2 in t2.attributes.keys(), "Wrong join attributes"
             assert funcName in functions, "Wrong join function"
-
+            
             res = [] # list of rows, rows: {attrname: value}
-            for i in range(t1.rowsize):
-                # 获取第一张表的tuple
-                _a1, _t1, tuple1 = t1.getTuple(i)
-                for j in range(t2.rowsize):
-                    # 获取第二张表的tuple
-                    _a2, _t2, tuple2 = t2.getTuple(j)
+            # 如果两个表tuple数量差距十分大e.g |a1|<lg(|a2|) 用nested-loop
+            size1 = t1.attributes[attrName1].getSize()
+            size2 = t2.attributes[attrName2].getSize()
+            if size1 < math.log2(size2) or size2 < math.log2(size1):
+                # join using nested-loop
+                for i in range(t1.rowsize):
+                    # 获取第一张表的tuple
+                    _a1, _t1, tuple1 = t1.getTuple(i)
+                    for j in range(t2.rowsize):
+                        # 获取第二张表的tuple
+                        _a2, _t2, tuple2 = t2.getTuple(j)
+                        # 判断是否满足条件
+                        b = functions[funcName](tuple1[attrName1], tuple2[attrName2], False)
+                        if not b:
+                            continue
+                        # 如果满足条件
+                        else:
+                            tuple2.pop(attrName2)
+                            row = {}
+                            row.update(tuple1)
+                            row.update(tuple2)
+                            res.append(row)
+            else:
+                '''
+                join using merge-scan:
+                Get First Row T1R from Table1
+                Get First Row T2R from Table2
+                While Not End of Table1 and Not End of Table2
+                    If T1R joins with T2R
+                        Returns (T1R, T2R)
+                        Get Next Row from Table2
+                    Else T1R < T2R
+                        Get NEXT Row from Table1
+                    Else
+                        Get Next Row from Table2
+                End Loop
+                '''
+                sortedIndices1 = t1.attributes[attrName1].getSortedIndexList()
+                sortedIndices2 = t2.attributes[attrName2].getSortedIndexList()
+                i = j = 0
+                while i<len(sortedIndices1) and j<len(sortedIndices2):
+                    _a1, _t1, tuple1 = t1.getTuple(sortedIndices1[i])
+                    _a2, _t2, tuple2 = t2.getTuple(sortedIndices2[j])
                     # 判断是否满足条件
                     b = functions[funcName](tuple1[attrName1], tuple2[attrName2], False)
-                    if not b:
-                        continue
-                    # 如果满足条件
-                    else:
+                    if b:
                         tuple2.pop(attrName2)
                         row = {}
                         row.update(tuple1)
                         row.update(tuple2)
                         res.append(row)
+                        j += 1
+                    elif tuple1[attrName1] < tuple2[attrName2]:
+                        i += 1
+                    else:
+                        j += 1
+                    
+
+
 
             attrsAfterJoin = []
-            for attrname in table1.attributes:
-                attrsAfterJoin.append(table1.attributes[attrname].copyEmptyAttr())
-            for attrname in table2.attributes:
+            for attrname in t1.attributes:
+                attrsAfterJoin.append(t1.attributes[attrname].copyEmptyAttr())
+            for attrname in t2.attributes:
                 if attrname != attrName2:
-                    attrsAfterJoin.append(table2.attributes[attrname].copyEmptyAttr())
+                    attrsAfterJoin.append(t2.attributes[attrname].copyEmptyAttr())
 
             table = Table('join_result', attrsAfterJoin)
             for r in res:
                 table.addTuple(r)
             return table
+
 
     def addForeignKey(self, t1, a1, t2, a2):
         '''
@@ -145,47 +190,77 @@ if __name__ == "__main__":
     # d1.dropTable('t1')
     # print(d1)
 
-    a1 = Attribute(name="a1", type=AttrTypes.INT, key=[AttrKeys.PRIMARY])
-    a1.addValue(1)
-    a1.addValue(2)
-    a1.addValue(4)
-    a2 = Attribute(name="a2", type=AttrTypes.VARCHAR, key=[AttrKeys.NOT_NULL])
-    a2.addValue("h")
-    a2.addValue("e")
-    a2.addValue("l")
+    #====================================test1=========================================
+    # a1 = Attribute(name="a1", type=AttrTypes.INT, key=[AttrKeys.PRIMARY])
+    # a1.addValue(1)
+    # a1.addValue(2)
+    # a1.addValue(4)
+    # a2 = Attribute(name="a2", type=AttrTypes.VARCHAR, key=[AttrKeys.NOT_NULL])
+    # a2.addValue("h")
+    # a2.addValue("e")
+    # a2.addValue("l")
+    # a3 = Attribute(name="a3", type=AttrTypes.INT, key=[AttrKeys.PRIMARY])
+    # a3.addValue(1)
+    # a3.addValue(2)
+    # a3.addValue(9)
+    # a4 = Attribute(name="a4", type=AttrTypes.VARCHAR, key=[AttrKeys.NOT_NULL])
+    # a4.addValue("w")
+    # a4.addValue("o")
+    # a4.addValue("r")
+    # a5 = Attribute(name="a5", type=AttrTypes.INT, key=[AttrKeys.NOT_NULL])
+    # a5.addValue(3)
+    # a5.addValue(2)
+    # a5.addValue(2)
+
+    # t1 = Table('t1', [a1, a2])
+    # t2 = Table('t2', [a3, a4, a5])
+
+    # f1 = open("./data/t1.tb", "wb")
+    # pickle.dump(t1, f1)
+    # f1 = open("./data/t1.tb", "rb")
+    # t1 = pickle.load(f1)
+    # print(t1)
+
+    # d1.addTable(t1)
+    # d1.addTable(t2)
+
+    # statement = "t1, t2 on t1.a1 = t2.a3"
+    # p = parseJoins([statement])
+    # print(d1)
+    # print(d1.join(p))
+    # join_table = d1.join(p)
+    # select_table = join_table.select("*", [[['a1',condition('inside', (1, 2))]]])
+    # print(select_table)
+
+    # d1.addForeignKey("t1", "a1", "t2", "a3")
+    # d1.addForeignKey("t2", "a3", "t1", "a2")
+    # print(d1.showForeignKeys())
+
+
+    # ===============================test join time===================================
+    a1 = Attribute(name="a1", type=AttrTypes.INT, key=[AttrKeys.NOT_NULL])
+    for i in range(1,1001):
+        a1.addValue(i)
+    a2 = Attribute(name="a2", type=AttrTypes.INT, key=[AttrKeys.NOT_NULL])
+    for i in range(1,1001):
+        a2.addValue(i)
     a3 = Attribute(name="a3", type=AttrTypes.INT, key=[AttrKeys.PRIMARY])
-    a3.addValue(1)
-    a3.addValue(2)
-    a3.addValue(9)
-    a4 = Attribute(name="a4", type=AttrTypes.VARCHAR, key=[AttrKeys.NOT_NULL])
-    a4.addValue("w")
-    a4.addValue("o")
-    a4.addValue("r")
-    a5 = Attribute(name="a5", type=AttrTypes.INT, key=[AttrKeys.NOT_NULL])
-    a5.addValue(2)
-    a5.addValue(2)
-    a5.addValue(2)
+    for i in range(1,10001):
+        a3.addValue(i)
+    a4 = Attribute(name="a4", type=AttrTypes.INT, key=[AttrKeys.NOT_NULL])
+    for i in range(1,10001):
+        a4.addValue(i)
 
     t1 = Table('t1', [a1, a2])
-    t2 = Table('t2', [a3, a4, a5])
-
-    f1 = open("./data/t1.tb", "wb")
-    pickle.dump(t1, f1)
-    f1 = open("./data/t1.tb", "rb")
-    t1 = pickle.load(f1)
-    print(t1)
+    t2 = Table('t2', [a3, a4])
 
     d1.addTable(t1)
     d1.addTable(t2)
 
-    statement = "t1, t2 on t1.a1 = t2.a5"
+    statement = "t1, t2 on t1.a1 = t2.a3"
     p = parseJoins([statement])
-    print(d1)
-    print(d1.join(p))
-    join_table = d1.join(p)
-    select_table = join_table.select("*", [[['a3',condition('inside', (1, 2))]]])
-    print(select_table)
 
-    d1.addForeignKey("t1", "a1", "t2", "a3")
-    d1.addForeignKey("t2", "a3", "t1", "a2")
-    print(d1.showForeignKeys())
+    time1 = time.time()
+    join_table = d1.join(p)
+    time2 = time.time()
+    print(time2-time1)
