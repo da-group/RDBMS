c79388531dd4c97498b71ce3c3938aac419b018c
rivenseiun <573935954@qq.com>
2019-12-03 01:24:17 -0500

    insert update delete

--- SimpleSql.py
+++ SimpleSql.py
@@ -1,8 +1,10 @@
 import os
+import traceback
 from Parser import parseCondition
 from Parser import parse
 from Database import *
 from Table import *
+from datetime import datetime
 
 
 class SimpleSql(object):
@@ -29,7 +31,7 @@ class SimpleSql(object):
 
 
   def _save_database(self, path):
-    pass
+       self.database.saveToFile(path)
 
  
   
@@ -48,29 +50,52 @@ class SimpleSql(object):
   def _drop(self, res):
       self.database.dropTable(res['tablename'])
   def _select(self, res):
-      pass
+      
+      
+      
+      
+      
+      
+      
+      
   def _insert(self, res):
       table = self.database.getTableByName(res['tablename'])
-      print(res['attrs_values'])
+#      print(table.attributes.keys())
+#      print(res['attrs_values'].keys())
+      for key in res['attrs_values'].keys():
+          a_type = table.attributes[key].getType().lower()
+          
+          if(a_type == "int"):
+              res['attrs_values'][key] = int(res['attrs_values'][key])
+          elif(a_type == "float"):
+              res['attrs_values'][key] = float(res['attrs_values'][key])
+          elif(a_type == "date"):
+              res['attrs_values'][key] = datetime.strptime(res['attrs_values'][key])
+        
       table.addTuple(res['attrs_values'])
-      
+      print(table)
       
       
   def _update(self,res):
-      pass
+      table = self.database.getTableByName(res['tablename'])
+      conditions = res['conditions']
+      row = res['assignments']
+      table.update(conditions, row)
+      print(table)
   
   def _delete(self, res):
-    pass
+    table = self.database.getTableByName(res['froms'][0])
+    if('conditions' in res.keys()):
+        conditions = res['conditions']
+        table.delete(conditions)
+    else: table.delete()
+    
+    print(table)
+    
+  def _show(self):
+    print(self.database)
 
 
-  def _show(self, res):
-    pass
-
-
-  
-
-
-  
 
 
 
@@ -86,22 +111,27 @@ class SimpleSql(object):
       statement = input('SimpleSql> ')
 #      print(statement)
       if(statement.lower().strip() == "exit"): break
-      res = parse(statement)
-      if(res["action_type"] == "create"):
-          self._create(res)
-      elif(res["action_type"] == "drop"):
-          self._drop(res)
-      elif("alter" in res["action_type"]):
-          self._alter(res)
-      elif(res["action_type"] == "select"):
-          self._select(res)
-      elif(res["action_type"] == "insert"):
-          self._insert(res)
-      elif(res["action_type"] == "update"):
-          self._update(res)
-      elif(res["action_type"] == "delete"):
-            self._delete(res)
-      print(self.database)
+      try:
+          if(statement.lower().strip() == "show"):
+              self._show()
+              continue
+          res = parse(statement)
+          if(res["action_type"] == "create"):
+              self._create(res)
+          elif(res["action_type"] == "drop"):
+              self._drop(res)
+          elif("alter" in res["action_type"]):
+              self._alter(res)
+          elif(res["action_type"] == "select"):
+              self._select(res)
+          elif(res["action_type"] == "insert"):
+              self._insert(res)
+          elif(res["action_type"] == "update"):
+              self._update(res)
+          elif(res["action_type"] == "delete"):
+                self._delete(res)
+      except:
+            print(traceback.format_exc())
         #print(res)
  
 
