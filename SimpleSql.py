355c3669008e7cc2a6503d2f61f2c53b3ac3764b
rivenseiun <573935954@qq.com>
2019-12-03 17:20:44 -0500

    Add files via upload

--- SimpleSql.py
+++ SimpleSql.py
@@ -5,7 +5,8 @@ from Parser import parse
 from Database import *
 from Table import *
 from datetime import datetime
-
+from Statistic import *
+import re
 
 class SimpleSql(object):
   '''
@@ -37,6 +38,7 @@ class SimpleSql(object):
   
   def _create(self,res):
       self.database.createTable(res['tablename'],res['attrs'])
+      print(res['foreign_key'])
       if("foreign_key") in res.keys():
           for f in res["foreign_key"]:
               assert len(f)==4,"Wrong foreign key"
@@ -59,22 +61,46 @@ class SimpleSql(object):
       tables = res['froms']
       attrs = res['attrs']
       conditions = res['conditions']
+      results = []
+      op = ""
       
-      table_condition = {}
-      table_attrs = {}
       
-      #without aggregate operations
+      
+      
+
       for t in tables:
           assert t in self.database.tables.keys(), "No such table"
           a_list = []
+          c_list = []
           for a in attrs:
-              if(a in t.attributes.keys()):
+              if(re.fullmatch(r'.{3}\(.*\)',a) or re.fullmatch(r'.{5}\(.*\)',a)):
+                  assert len(attrs)==1,"invalid nums of attributes"
+                  attr =re.findall(r'[(](.*?)[)]',a)[0].strip()
+                  a_list.append(attr)
+                  op = a.replace(re.findall(r'\(.*?\)',a)[0],'')
+                  
+              elif(a in t.attributes.keys()):
                   a_list.append(a)
               elif a == "*":
                   a_list = t.attributes.keys()
+              elif('.' in a):
+                  if(a.split('.')[0] == t and a.split(".")[1] in t.attributes.keys()):
+                      a_list.append(a.split(".")[1])
+                      
                   
-          table_attrs[table.name] = a_list
-      
+          for c in conditions:
+              if(c[0] == t) :
+                  c_list.append(c)
+          result=t.select(a_list,c_list)
+          
+          if(op == ""):results.append(result)
+          else:
+              attr = result.getAttribute(a_list[0])
+              results.append(FuncMap[op](attr))
+              
+       
+        
+      print(results)
       
       
       
@@ -116,10 +142,26 @@ class SimpleSql(object):
     
     print(table)
     
+    
+  def _createdb(self,name):
+      self.database = Database(name)
+      
+      
   def _show(self):
     print(self.database)
 
 
+  def _showtable(self):
+      for k in self.database.tables.keys():
+          print(self.database.tables[k])
+
+  def _createindex(self,res):
+      table = self.database.getTableByName(res['tablename'])
+      attr = table.getAttribute(res['attr'])
+      attr.buildIndex(10)
+      
+      
+      
 
 
 
@@ -129,8 +171,6 @@ class SimpleSql(object):
 
   
   def run(self):
-    self.database = Database("db1") 
-    print(self.database)
     while True:
       statement = input('SimpleSql> ')
 #      print(statement)
@@ -139,21 +179,40 @@ class SimpleSql(object):
           if(statement.lower().strip() == "show"):
               self._show()
               continue
+          if(statement.lower().strip() == "show table"):
+              self._showtable()
+              continue
+          
           res = parse(statement)
           if(res["action_type"] == "create"):
+              assert self.database != None,"NO DATABASE!"
               self._create(res)
           elif(res["action_type"] == "drop"):
+              assert self.database != None,"NO DATABASE!"
               self._drop(res)
           elif("alter" in res["action_type"]):
+              assert self.database != None,"NO DATABASE!"
               self._alter(res)
           elif(res["action_type"] == "select"):
+              assert self.database != None,"NO DATABASE!"
               self._select(res)
           elif(res["action_type"] == "insert"):
+              assert self.database != None,"NO DATABASE!"
               self._insert(res)
           elif(res["action_type"] == "update"):
+              assert self.database != None,"NO DATABASE!"
               self._update(res)
           elif(res["action_type"] == "delete"):
-                self._delete(res)
+              assert self.database != None,"NO DATABASE!"
+              self._delete(res)
+          elif(res["action_type"] == "create index"):
+              assert self.database != None,"NO DATABASE!"
+              self._createindex(res)
+              
+          elif(res["action_type"] == "create db"):
+              self._createdb(res["database_name"])
+              self._show()
+          
       except:
             print(traceback.format_exc())
         #print(res)
